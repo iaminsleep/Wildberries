42f61e0a99dbd49f2ec6ea4d0bdacaebe5a0f143
iaminsleep <ruslanergashevv@yandex.ru>
2021-11-23 00:13:41 +0300

    added smooth scroll script

---
+++ js/smoothScroll.js
@@ -0,0 +1,15 @@
+const smoothScroll = () => {
+    const links = document.querySelectorAll('.scroll-link');
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        })
+    })
+}
+
+smoothScroll();
