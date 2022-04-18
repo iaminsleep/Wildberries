9a04e87139427a3f6ece056ae006b332cf6dcf7f
iaminsleep <xenydok1292@gmail.com>
2022-04-18 19:12:20 +0300

    Full implementation of order management for managers

--- src/components/goods/orderitem.js
+++ src/components/goods/orderitem.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function OrderItem({API, img, price, quantity, name, total}) {
   return (
     <tr>
-      <td className="w-10"><img src={`${API}/img/goods/${img}`} alt={`${name}`}/></td>
+      <td className="w-8"><img src={`${API}/img/goods/${img}`} alt={`${name}`}/></td>
       <td className="w-100">{name}</td>
       <td>${price}</td>
       <td>{quantity}</td>
--- src/pages/orderManager.js
+++ src/pages/orderManager.js
@@ -4,28 +4,29 @@ import axios from 'axios';
 
 import { setError } from '../.store/actions/setMessages';
 
-import Order from '../components/goods/order';
+import OrderItem from '../components/goods/orderItem';
 
 import '../css/pages/order_manager.css';
 
 function OrderManager({API, getCookie}) {
   const [orders, setOrders] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
 
   const dispatch = useDispatch();
 
+  const accessToken = getCookie('accessToken');
+
   useEffect(() => {
     const getOrders = async() => {
-      const accessToken = getCookie('accessToken');
       if(!accessToken) return false;
       else {
         await axios.get(`${API}/orders`, { 
           headers: { 'Authorization': 'Bearer ' + accessToken }
         }).then((response) => {
-          console.log(response);
-          console.log(response.data);
           let status = response.status;
           if(status === 200) {
-            dispatch(setOrders(response.data));
+            setOrders(response.data);
+            setLoaded(true);
           }
           else {
             dispatch(setError(response.data.message));
@@ -34,20 +35,77 @@ function OrderManager({API, getCookie}) {
       }
     }
 
-    getOrders();
-  }, []);
+    if(!isLoaded) {
+      getOrders();
+    }
+  }, [isLoaded, API, dispatch, accessToken]);
 
   return (
     <React.Fragment>
-      {orders.length > 0 ? 
-        orders.map(order => {
-          <Order key={order.id} orderInfo={order.order_info} 
-          orderItems={order.order_items} userInfo={order.user_info}/>
-        })
-      : <div className="empty-goods-wrapper purple">
-          <div id="orders-empty">There are no active orders from users!</div>
-        </div>
-      }
+      <section className="orders">
+        {orders.length > 0 ? 
+          orders.map(order => {
+            return(
+              <div key={order.order_info.id} className="order">
+                <h2 className="order-number">Order &#8470; {order.order_info.id}</h2>
+                <h2 className="order-title">Details</h2>
+                <div className="order-info">
+                  <p className="order-info-description">Created: {order.order_info.created_at}</p>
+                  <p className="order-info-description">Comment:&nbsp;
+                    <span className={"comment" + (order.order_info.comment === '' ? " empty" : "")}>
+                      {order.order_info.comment !== '' 
+                      ? order.order_info.comment 
+                      : 'The user chose not to leave a comment'}
+                    </span>
+                  </p>
+                </div>
+                <h2 className="order-title">User Info</h2>
+                <div className="user-info">
+                  <p className="user-info-description">Name: {order.user_info.name}</p>
+                  <p className="user-info-description">Email: {order.user_info.email}</p>
+                  <p className="user-info-description">Phone:&nbsp;
+                    <a href={`tel:+${order.user_info.phone}`}>
+                      {order.user_info.phone}
+                    </a>
+                  </p>
+                </div>
+                <h2 className="order-title">Products:</h2>
+                <table className="cart-table" id="order-table">
+                  <colgroup>
+                    <col className="col-image"/>
+                    <col className="col-name"/>
+                    <col className="col-price"/>
+                    <col className="col-qty"/>
+                    <col className="col-total-price"/>
+                    <col className="col-delete"/>
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>Name</th>
+                      <th>Price</th>
+                      <th>Qty.</th>
+                      <th>Total</th>
+                    </tr>
+                  </thead>
+                  <tbody className="cart-table__goods">
+                    {order.order_items.map(orderItem => 
+                      <OrderItem
+                        key={orderItem.id} img={orderItem.img} name={orderItem.name} price={orderItem.price} 
+                        quantity={orderItem.quantity} total={orderItem.price * orderItem.quantity} API={API}
+                      />
+                    )}
+                  </tbody>
+                </table>
+                <p className="order-info-description total" id="order-total">Total: ${order.order_info.total}</p>
+              </div>
+            )
+          })
+        : <div className="empty-goods-wrapper purple">
+            <div id="orders-empty">There are no active orders from users!</div>
+          </div>
+        }
+      </section>
     </React.Fragment>
   );
 }
